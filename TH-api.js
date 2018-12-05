87cfd8e5a93d1fa7e5f80c9ad0aa4470b596d459
AKesidis <akesidis@uclan.ac.uk>
2018-12-05 23:07:06 +0200

    Update of Everything

--- TH-api.js
+++ TH-api.js
@@ -1,84 +1,219 @@
-
 function getChallenges(){
-    var challengesList=document.getElementById("Challenges");
+    var session = fromCookie("session");
+    if (session === null || session === undefined || session==="undefined") {
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function () {
+            if (this.readyState === 4 && this.status === 200) {
+                //TODO if response received (Success)
+                console.log(this.responseText);
+                object = JSON.parse(this.responseText);
+                var object;
+                var tHS = object.treasureHunts;
+                var challengesList = document.getElementById("Challenges");
+                var challengesForm = document.getElementById("challengesForm");
+                for (var i = 0; i < tHS.length; i++) {
+                    challengesList.innerHTML += '<li><input type="radio" name="treasure-hunt-id" ' +
+                        'value=' + tHS[i].uuid + '>' + tHS[i].name + '</li><br>';
+                }
+                challengesForm.innerHTML += '<input type="submit" value="Submit">';
+            }
+        };
+        xhttp.open("GET", "https://codecyprus.org/th/api/list", true);
+        xhttp.send();
+    }else {
+        window.location.href = "Questions.html";
+    }
+}
 
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange=function(){
+function getFromURL(param) {
+    let url = new URL(window.location.href);
+    return url.searchParams.get(param);
+}
 
-        if(this.readyState===4 && this.status===200){
-            //TODO if response received (Success)
-            console.log(this.responseText);
-             object = JSON.parse(this.responseText);
-            console.log("STATUS===>"+object.status);
+function question(){
+    var session = fromCookie("session");
+    if (session === null || session === undefined || session==="undefined") {
+        console.log("no session in cookie");
+        var player = getFromURL("player");
+        var thID = getFromURL("treasure-hunt-id");
+        console.log(player);
 
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function () {
+            if (this.readyState === 4 && this.status === 200) {
+                let obj = JSON.parse(this.responseText);
+                console.log(obj);
+                session = obj.session;
+                let time = new Date(Date.now()+1200000);
+                document.cookie = "session" + "=" + session + ";" + "expires=" + time.toUTCString();
+                loadQuestion();
+            }
+        };
+        xhttp.open("GET", "https://codecyprus.org/th/api/start?player=" + player + "&app=Simpsons&treasure-hunt-id=" + thID, true);
+        xhttp.send();
+    } else {
+        console.log("session "+session);
+        loadQuestion();
+    }
+}
 
-            var newITEM;
-            var object;
-
-            for(var i =0;i<object.treasureHunts.length;i++){
-
-                newITEM = document.createElement("li");
-
-                var linkItem=document.createElement("a");
-                linkItem.innerHTML=object.treasureHunts[i].name;
-                linkItem.href="https://codecyprus.org/th/api/start?player=Homer&app=Simpson&treasure-hunt-id=\" + object.treasureHunts[i].uuid;"
-                newITEM.appendChild(linkItem);
-
-                challengesList.appendChild(newITEM);
+function loadQuestion() {
+    score();
+    var qReq = new XMLHttpRequest();
+    qReq.onreadystatechange = function () {
+        if (this.readyState === 4 && this.status === 200) {
+            let qObj = JSON.parse(this.responseText);
+            console.log(qObj);
+            if (!qObj.completed) {
+                document.getElementById("question").innerHTML = qObj.questionText;
+                var answer = document.getElementById("answer");
+                var qAns = '<form id=answerForm action="Answer.html">';
+                if (qObj.questionType === "INTEGER" || qObj.questionType === "NUMERIC") {
+                    qAns +=
+                        '<input type="number" name="answer">';
+                } else if (qObj.questionType === "MCQ") {
+                    qAns +=
+                        '<input type="radio" name="answer" value="a">A' +
+                        '<input type="radio" name="answer" value="b">B' +
+                        '<input type="radio" name="answer" value="c">C' +
+                        '<input type="radio" name="answer" value="d">D' ;
+                } else if (qObj.questionType === "BOOLEAN") {
+                    qAns +=
+                        '<input type="radio" name="answer" value="true"> True' +
+                        '<input type="radio" name="answer" value="false"> False';
+                } else if (qObj.questionType === "TEXT") {
+                    qAns +=
+                        '<input type="text" name="answer" placeholder="answer">' ;
+                }
+                qAns +=
+                    '<br><br><input type="submit">' +
+                    '</form>';
+                if (qObj.canBeSkipped) {
+                    qAns += '<br><input type="button" value="Skip" onclick="skip()"> ';
+                }
+                answer.innerHTML = qAns;
+            }else {
+                window.location.href = "Leaderboard.html";
             }
         }
-        else{
-            //TODO if response not received(error)
+    };
+    qReq.open("GET", "https://codecyprus.org/th/api/question?session=" + fromCookie("session"), true);
+    qReq.send();
+}
+
+function skip() {
+    var sReq = new XMLHttpRequest();
+    sReq.onreadystatechange = function (){
+        if (this.readyState === 4 && this.status === 200) {
+            loadQuestion();
         }
     };
-    xhttp.open("GET","https://codecyprus.org/th/api/list",true);
+    sReq.open("GET","https://codecyprus.org/th/api/skip?session="+fromCookie("session"));
+    sReq.send();
+}
+
+function answer() {
+    var answer = getFromURL("answer");
+    console.log(answer);
+    var aHTTP = new XMLHttpRequest();
+    aHTTP.onreadystatechange = function(){
+        if (this.readyState === 4 && this.status === 200) {
+            var aObj = JSON.parse(this.responseText);
+            console.log(aObj);
+            var result = document.getElementById("result");
+            if (aObj.correct) {
+                result.innerHTML = 'Correct<br>';
+            }else {
+                result.innerHTML = 'Incorrect<br>'
+            }
+            result.innerHTML += '<form action="Questions.html"> ' +
+                '<input type="submit"/>'+
+                '</form>';
+        }
+    };
+    aHTTP.open("GET", "https://codecyprus.org/th/api/answer?session="+fromCookie("session")+"&answer=" + answer, true);
+    aHTTP.send();
+}
+
+function deleteCookie() {
+    let expir = new Date(Date.now() - 10000);
+    let keyValue = document.cookie.split(";");
+    for (let i = 0; i < keyValue.length; i++) {
+        let key = keyValue[i].split("=")[0].trim();
+        document.cookie = key + "=; " + "expires=" + expir.toUTCString() + ";";
+    }
+}
+
+function fromCookie(property) {
+    let keyValue = document.cookie.split(";");
+    for (let i = 0; i < keyValue.length; i++) {
+        let key = keyValue[i].split("=")[0].trim();
+        let val = keyValue[i].split("=")[1];
+        if (property===key) return val;
+    }
+}
+
+function leaderBoard() {
+    var lReq = new XMLHttpRequest();
+    lReq.onreadystatechange = function(){
+        if (this.readyState === 4 && this.status === 200) {
+            var obj = JSON.parse(this.responseText);
+            console.log(obj);
+            var leadList = document.getElementById("leaderList");
+            var leadArray = obj.leaderboard;
+            let options = { day: 'numeric', month: 'short', hour: '2-digit',minute: '2-digit', second: '2-digit' };
+            for (let i=0;i<leadArray.length;i++){
+                let date = new Date(leadArray[i].completionTime);
+                let formattedDate = date.toLocaleDateString("en-UK", options);
+                leadList.innerHTML += '<li>'+ leadArray[i].player + " " + leadArray[i].score + " " + formattedDate+"</li>";
+            }
+        }
+    };
+    lReq.open("GET", "https://codecyprus.org/th/api/leaderboard?session=" + fromCookie("session") + "&limit=20&sorted", true);
+    lReq.send();
+}
+
+function score() {
+    var session = fromCookie("session");
+    if (session !== null || session !== undefined || session !=="undefined") {
+        var scReq = new XMLHttpRequest();
+        scReq.onreadystatechange = function(){
+            if (this.readyState === 4 && this.status === 200) {
+                var scObj = JSON.parse(this.responseText);
+                var score = document.getElementById("score");
+                score.innerHTML = "Score: "+scObj.score;
+            }
+        };
+        scReq.open("GET", "https://codecyprus.org/th/api/score?session=" + fromCookie("session"), true);
+        scReq.send();
+    }
+}
+
+//Get the location from the client.
+function getLocation() {
+    navigator.geolocation.getCurrentPosition(showPosition);
+    function showPosition(position) {
+        console.log("latitude: " + position.coords.latitude); //To check if the location is get by the server
+        console.log("longitude: " + position.coords.longitude);
+        sendLocation(position.coords.latitude, position.coords.longitude);
+    }
+}
+
+//Send the location to the server.
+function sendLocation(latitude,longitude) {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState === 4 && this.status === 200) {
+            //TODO If response received (success).
+            console.log("location received!");
+        }
+        else {
+            //TODO If response not received (error).
+        }
+    };
+    xhttp.open("GET", "https://codecyprus.org/th/api/location?" +
+        "session=" + cookie("session") + "&latitude=" + latitude + "&longitude=" + longitude, true);
     xhttp.send();
 }
+getLocation();
 
-
-
-function info(){
-
-    var F_PlayerName = document.getElementById("PlayerName").value;
-    var F_Email = document.getElementById("Email").value;
-
-    if (F_PlayerName==="" || F_Email==="")
-    {
-        alert("ERROR");
-    }else {
-         if(window.sessionStorage) {
-             /*Go to questions*/
-             F_PlayerName.value = sessionStorage.getItem("PlayerName");//get Form Elements
-             F_Email.value = sessionStorage.getItem("Email");
-
-             F_PlayerName.addEventListener("input", function () {
-                 sessionStorage.setItem("PlayerName", F_PlayerName.value);
-             }, false);
-             F_Email.addEventListener("input", function () {
-                 sessionStorage.setItem("Email", F_Email.value);
-             }, false);
-         }
-        location.href="Questions.html";
-
-
-    }
-
-}
-
-
-
-function start(){
-    var xhttp=new XMLHttpRequest();
-    xhttp.onreadystatechange=function(){
-        if(this.readyState===4 && this.status===200){
-            console.log(this.responseText);
-            var object=JSON.parse(this.responseText);
-            console.log("STATUS===>"+object.status);
-
-
-        }
-    }
-
-
-
-}
